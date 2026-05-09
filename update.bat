7f3e3f28b946f8b31eca8253c14dea1dfcc12aa6
Phogapro <kim915801@gmail.com>
2026-05-09 19:05:11 +0700

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,4 +1,66 @@
+@echo off
+
+title Cloud Phone Config Manager
+
+echo =========================
+echo UPDATE START
+echo =========================
+
+echo.
+echo [1/7] Checkout main branch...
+git checkout main
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo CHECKOUT MAIN FAILED
+    pause
+    exit /b
+)
+
+echo.
+echo [2/7] Pull latest from GitHub...
+git pull origin main
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo GIT PULL FAILED
+    pause
+    exit /b
+)
+
+echo.
+echo [3/7] Generate YAML...
 python generate.py
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo GENERATE FAILED
+    pause
+    exit /b
+)
+
+echo.
+echo [4/7] Add files...
 git add .
-git commit -m update
-git push
+
+echo.
+echo [5/7] Commit...
+git commit -m "auto update"
+
+echo.
+echo [6/7] Push...
+git push origin main
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo PUSH FAILED
+    pause
+    exit /b
+)
+
+echo.
+echo =========================
+echo UPDATE SUCCESS
+echo =========================
+
+pause
